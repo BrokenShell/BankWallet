b20b071b37d0cf48f20e491c9f267edce9211e12
BrokenShell <webmaster@sharpdesigndigital.com>
2022-11-04 09:55:30 -0700

    adds ability to sort wallets

--- wallet/main.py
+++ wallet/main.py
@@ -2,25 +2,46 @@ from pydantic import conint
 
 
 class BankWallet:
+    # Todo: Add overdraft charge logic
 
     def __init__(self, amount: conint(gt=0)):
+        #Todo: Protect from negative amounts
         self.balance = amount
 
     def deposit(self, amount: conint(gt=0)):
+        # Todo: Protect from negative amounts
         self.balance += amount
 
     def debit(self, amount: conint(gt=0)):
+        # Todo: Protect from negative amounts
         self.balance -= amount
 
     def __str__(self):
-        return "\n".join(f"{k}: {v}" for k, v in vars(self).items())
+        return "\n".join(f"{k} => {v}" for k, v in vars(self).items())
+
+    def __repr__(self):
+        return str(self)
+
+    def __lt__(self, other):
+        return self.balance < other.balance
 
 
 w1 = BankWallet(10)
 w2 = BankWallet(100)
 w3 = BankWallet(-100)
+arr = [w1, w2, w3]
+arr.sort()
+print(arr)
 
 
-print(w1)
-print(w2)
-print(w3)
+"""
+def combat_round(character, target):
+    if character is instance_of(Caster):
+        character.spell(target)
+    elif character is instance_of(Melee):
+        character.swing(target)
+
+def combat_round_poly(character, target):
+    character.attack(target)
+
+"""
